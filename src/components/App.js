3e2ffe3134230753ca1fa38007149f0c5a033782
Georgi Mirchev <gmirchev90@gmail.com>
2018-02-04 16:01:19 +0200

    Init commit with rxjs and simple components

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-named-as-default */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, NavLink, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import NotFoundPage from './NotFoundPage';
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+
+class App extends React.Component {
+  render() {
+    const activeStyle = { color: 'blue' };
+    return (
+      <div>
+        <div>
+          <NavLink exact to="/" activeStyle={activeStyle}>Срещи</NavLink>
+          {' | '}
+          <NavLink to="/login" activeStyle={activeStyle}>Вход</NavLink>
+        </div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.element
+};
+
+export default App;
