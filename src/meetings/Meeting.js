36a2cb32dfb30a8cf2a7497adffc6b0065ad5204
Georgi Mirchev <georgi.mirchev@estafet.com>
2018-04-04 16:45:14 +0300

    Use moment.js to format the date

--- src/meetings/Meeting.js
+++ src/meetings/Meeting.js
@@ -1,7 +1,14 @@
 import React, {Component} from "react";
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import {Card, CardHeader, CardTitle, CardText} from 'material-ui/Card';
 import Linkify from 'react-linkify';
+
+const style = {
+  marginTop: '10px',
+  marginLeft: '30px',
+  marginRight: '30px'
+};
 
 class Meeting extends Component {
 
@@ -9,23 +16,25 @@ class Meeting extends Component {
     const meeting = this.props.meeting;
     const organizers = meeting.organizers.map(organizer =>
       <CardHeader
+        key={organizer._id}
         title={organizer.name}
         subtitle="Subtitle"
         avatar={organizer.profilePicture}
       />
     );
+    let index = 0;
     const description = meeting.description.split("\n").map(item => {
       return (
-        <span>
+        <span key={index++}>
                 {item}
           <br/>
         </span>
-      )
+      );
     });
     return (
-      <Card>
+      <Card style={style}>
         {organizers}
-        <CardTitle title={meeting.name} subtitle="Card subtitle"/>
+        <CardTitle title={meeting.name} subtitle={moment(meeting.start_time).format("DD MMMM, YYYY")}/>
         <CardText>
           <Linkify>
             {description}
