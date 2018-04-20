34862d685f07337736e7fb8f413b24eb160cf6b6
Georgi Mirchev <gmirchev90@gmail.com>
2018-04-20 14:59:32 +0300

    HTTPS connection

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = 'http://haide-na-razhodka-server.herokuapp.com/api';
+const SERVER_URL = 'https://haide-na-razhodka-server.herokuapp.com/api';
 
 export async function getMeetings(page, pageSize, fromDate, toDate) {
   return axios.get(`${SERVER_URL}/meetings?page=${page}&pageSize=${pageSize}&fromDate=${fromDate}&toDate=${toDate}`);
