3ce2374c8fd718730246f83ee7741b606bcffb46
Georgi Mirchev <gmirchev90@gmail.com>
2018-04-27 19:18:46 +0300

    Fixed authentication with Facebook to use TourMeet data

--- src/login/LoginDialog.js
+++ src/login/LoginDialog.js
@@ -2,10 +2,32 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Dialog from 'material-ui/Dialog';
 
-const customContentStyle = {
-};
-
 class LoginDialog extends Component {
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId            : '328009247324005',
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v2.12'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=874212442599315&autoLogAppEvents=1';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  login() {
+    window.FB.login(function(response) {
+      console.log(response);
+    }, {scope: 'email'});
+  }
+
   render() {
 
     const open = this.props.open;
@@ -14,12 +36,13 @@ class LoginDialog extends Component {
     return (
       <Dialog
         title="Вход с Facebook"
+        style={{height: "600px"}}
         modal={false}
-        contentStyle={customContentStyle}
         open={open}
         onRequestClose={onRequestClose}
       >
-        This dialog spans the entire width of the screen.
+        <div id={'fb-root'} />
+        <button onClick={() => this.login()}>Login with Facebook </button>
       </Dialog>
     );
   }
