e679cfe480f52b7bda234528f37a8dee10eff5f2
Georgi Mirchev <georgi.mirchev@estafet.com>
2018-05-07 08:33:02 +0300

    Rename components to root

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './store/configureStore';
-import Root from './components/Root';
+import Root from './root/Root';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
@@ -18,8 +18,8 @@ render(
 );
 
 if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-    const NewRoot = require('./components/Root').default;
+  module.hot.accept('./root/Root', () => {
+    const NewRoot = require('./root/Root').default;
     render(
       <AppContainer>
         <NewRoot store={store} history={history} />
